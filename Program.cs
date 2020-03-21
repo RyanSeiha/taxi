e0a903c7e1fb53cfb7765a6a9a5766778d1044ae
ryan.aryanatama@students.amikom.ac.id <ryan.aryanatama@students.amikom.ac.id>
2020-03-21 19:24:12 +0700

    Membuat Program

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Taxi taxi = new Taxi
+            {
+                DriverName = "Juana",
+                OnDuty = true,
+                NumbOfPassenger = 40
+            };
+
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOfPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
